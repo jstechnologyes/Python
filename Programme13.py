fc16b44b375698734b3f1d1231cef05f342bffd2
Hadiuzzaman <38618540+jstechnologyes@users.noreply.github.com>
2019-11-06 23:23:23 +0600

    Largest Number

---
+++ Programme13.py
@@ -0,0 +1,16 @@
+# Largest Number
+num1=30
+num2=60
+num3=40
+
+if num1>num2:
+    if num1>num3:
+        print(num1)
+    else:
+        print(num3)
+
+if num2>num1:
+    if num2>num3:
+        print(num2)
+    else:
+        print(num3)
