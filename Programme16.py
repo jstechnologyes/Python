bbdb334372aa314b792ddda65819779cfb8bdebd
Hadiuzzaman <38618540+jstechnologyes@users.noreply.github.com>
2019-11-06 23:51:45 +0600

    Grade Point Statement

---
+++ Programme16.py
@@ -0,0 +1,24 @@
+
+
+marks=55
+if 80 <= marks <=100:
+    print ("A+")
+
+elif 70 <= marks <=79:
+    print ("A")
+
+elif 60 <= marks <=69:
+    print ("A-")
+
+elif 50 <= marks <=59:
+    print ("B")
+
+elif 40 <= marks <=49:
+    print ("C")
+
+elif 33 <= marks <=39:
+    print ("D")
+
+else:
+    print("Fail")
+
