08abfea5a8a81e75b68f991310483f9252f0a7db
Hadiuzzaman <38618540+jstechnologyes@users.noreply.github.com>
2019-11-06 23:34:38 +0600

    Logical Opearator

---
+++ Programme15.py
@@ -0,0 +1,20 @@
+#Logical Opearator
+#Largest Number
+num1=20
+num2=10
+num3=90
+
+if num1>num2 and num1>num3:
+    print(num1)
+elif num2>num1 and num2>num3:
+    print(num2)
+else:
+    print(num3)
+
+#vowel and Consonant
+
+ch='a'
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u':
+    print("Vowel")
+else:
+    print("Consonant")
